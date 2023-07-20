1873b75004c701bd38cd143d43ff13b441c334f8
p3podev <jr_garciaparedes@hotmail.com>
2023-07-20 12:19:18 -0500

    Feat Select random enemy's pet with case on JS and view on HTML file

--- js/mokepon.js
+++ js/mokepon.js
@@ -2,7 +2,9 @@ function startGame() {
     let buttonPet = document.getElementById('button_pet')
     buttonPet.addEventListener('click', selectPetPlayer)
 }
-
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 function selectPetPlayer() {
     let radioHipodoge = document.getElementById('hipodoge')
     let radioCapipepo = document.getElementById('capipepo')
@@ -10,23 +12,47 @@ function selectPetPlayer() {
     let radioLangostelvis = document.getElementById('langostelvis')
     let radioTucapalma = document.getElementById('tucapalma')
     let radioPydos = document.getElementById('pydos')
-    let spanPetPlayer=document.getElementById('pet_Player')
+    let spanPetPlayer = document.getElementById('pet_Player')
     if (radioHipodoge.checked) {
-        spanPetPlayer.innerHTML=' Hipodoge '
+        spanPetPlayer.innerHTML = ' Hipodoge '
     } else if (radioCapipepo.checked) {
-        spanPetPlayer.innerHTML=' Capipepo '
+        spanPetPlayer.innerHTML = ' Capipepo '
     } else if (radioRatigueya.checked) {
-        spanPetPlayer.innerHTML=' Ratigueya '
+        spanPetPlayer.innerHTML = ' Ratigueya '
     } else if (radioLangostelvis.checked) {
-        spanPetPlayer.innerHTML=' Langostelvis '
+        spanPetPlayer.innerHTML = ' Langostelvis '
     } else if (radioTucapalma.checked) {
-        spanPetPlayer.innerHTML=' Tucapalma '
+        spanPetPlayer.innerHTML = ' Tucapalma '
     } else if (radioPydos.checked) {
-        spanPetPlayer.innerHTML=' Pydos '
-    }else {
+        spanPetPlayer.innerHTML = ' Pydos '
+    } else {
         alert("Selecciona primero una Pet")
     }
+    selectPetEnemy()
 
 }
-
+function selectPetEnemy() {
+    let petRandom = aleatorio(1, 6)
+    let spanPetEnemy = document.getElementById('pet_Enemy')
+    switch (petRandom) {
+        case 1:
+            spanPetEnemy.innerHTML = ' Hipodoge '
+            break;
+        case 2:
+            spanPetEnemy.innerHTML = ' Capipepo '
+            break;
+        case 3:
+            spanPetEnemy.innerHTML = ' Ratigueya '
+            break;
+        case 4:
+            spanPetEnemy.innerHTML = ' Langostelvis '
+            break;
+        case 5:
+            spanPetEnemy.innerHTML = ' Tucapalma '
+            break;
+        case 6:
+            spanPetEnemy.innerHTML = ' Pydos '
+            break;
+    }
+}
 window.addEventListener('load', startGame)

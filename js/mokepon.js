6ff536e3a0c4f8f30be7e5afd1972adfddf2c9ad
p3podev <jr_garciaparedes@hotmail.com>
2023-12-29 23:41:23 -0500

    Refactor: In Js cut all variables gElbyId to top and Change let varibles for const, in Html change possition of js script

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,21 +1,35 @@
+const selectAttackSection = document.getElementById('select_attack')
+const restartSection = document.getElementById('restart')
+const buttonPet = document.getElementById('button_pet')
+const buttonFire = document.getElementById('button_fire')
+const buttonWater = document.getElementById('button_water')
+const buttonEarth = document.getElementById('button_earth')
+const buttonRestart = document.getElementById('button_reset')
+const selectPetSection = document.getElementById('select_pet')
+const radioHipodoge = document.getElementById('hipodoge')
+const radioCapipepo = document.getElementById('capipepo')
+const radioRatigueya = document.getElementById('ratigueya')
+const radioLangostelvis = document.getElementById('langostelvis')
+const radioTucapalma = document.getElementById('tucapalma')
+const radioPydos = document.getElementById('pydos')
+const spanPetPlayer = document.getElementById('pet_Player')
+const spanPetEnemy = document.getElementById('pet_Enemy')
+const sectionMessages = document.getElementById('resultHTML')
+const playersAttack = document.getElementById('players_attack')
+const enemysAttack = document.getElementById('enemys_attack')
+const spanplayerLives = document.getElementById('player_Lives')
+const spanenemyLives = document.getElementById('enemy_Lives')
 let attackPlayer = ''
 let attackEnemy = ''
 let playerLives = 3
 let enemyLives = 3
 function startGame() {
-    let selectAttackSection = document.getElementById('select_attack')
     selectAttackSection.style.display = 'none'
-    let restartSection = document.getElementById('restart')
     restartSection.style.display = 'none'
-    let buttonPet = document.getElementById('button_pet')
     buttonPet.addEventListener('click', selectPetPlayer)
-    let buttonFire = document.getElementById('button_fire')
     buttonFire.addEventListener('click', attackFire)
-    let buttonWater = document.getElementById('button_water')
     buttonWater.addEventListener('click', attackWater)
-    let buttonEarth = document.getElementById('button_earth')
     buttonEarth.addEventListener('click', attackEarth)
-    let buttonRestart = document.getElementById('button_reset')
     buttonRestart.addEventListener('click', resetGame)
 }
 function resetGame() {
@@ -25,17 +39,8 @@ function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 function selectPetPlayer() {
-    let selectAttackSection = document.getElementById('select_attack')
     selectAttackSection.style.display = 'flex'
-    let selectPetSection = document.getElementById('select_pet')
     selectPetSection.style.display = 'none'
-    let radioHipodoge = document.getElementById('hipodoge')
-    let radioCapipepo = document.getElementById('capipepo')
-    let radioRatigueya = document.getElementById('ratigueya')
-    let radioLangostelvis = document.getElementById('langostelvis')
-    let radioTucapalma = document.getElementById('tucapalma')
-    let radioPydos = document.getElementById('pydos')
-    let spanPetPlayer = document.getElementById('pet_Player')
     if (radioHipodoge.checked) {
         spanPetPlayer.innerHTML = ' Hipodoge '
     } else if (radioCapipepo.checked) {
@@ -52,7 +57,6 @@ function selectPetPlayer() {
         alert("Selecciona primero una Pet")
     }
     selectPetEnemy()
-    let buttonPet = document.getElementById('button_pet')
     buttonPet.disabled = true
     radioHipodoge.disabled = true
     radioCapipepo.disabled = true
@@ -63,7 +67,6 @@ function selectPetPlayer() {
 }
 function selectPetEnemy() {
     let petRandom = aleatorio(1, 6)
-    let spanPetEnemy = document.getElementById('pet_Enemy')
     switch (petRandom) {
         case 1:
             spanPetEnemy.innerHTML = ' Hipodoge '
@@ -108,36 +111,22 @@ function reviewLives() {
     }
 }
 function createFinalMessage(finalResult) {
-    let sectionMessages = document.getElementById('resultHTML')
-    sectionMessages .innerHTML = finalResult
-    
-
-    let buttonFire = document.getElementById('button_fire')
+    sectionMessages.innerHTML = finalResult
     buttonFire.disabled = true
-    let buttonWater = document.getElementById('button_water')
     buttonWater.disabled = true
-    let buttonEarth = document.getElementById('button_earth')
     buttonEarth.disabled = true
-    let restartSection = document.getElementById('restart')
     restartSection.style.display = 'block'
 }
 function createMessage(result) {
-    let sectionMessages = document.getElementById('resultHTML')
-    let playersAttack = document.getElementById('players_attack')
-    let enemysAttack = document.getElementById('enemys_attack')
     let newPlayerAttack = document.createElement('p')
     let newEnemyAttack = document.createElement('p')
-
     sectionMessages.innerHTML = result
     newPlayerAttack.innerHTML = attackPlayer
     newEnemyAttack.innerHTML = attackEnemy
-
     playersAttack.appendChild(newPlayerAttack);
     enemysAttack.appendChild(newEnemyAttack);
 }
 function combat() {
-    let spanplayerLives = document.getElementById('player_Lives')
-    let spanenemyLives = document.getElementById('enemy_Lives')
     if (attackPlayer == attackEnemy) {
         createMessage('😐 YOU DRAW 😐')
     } else if ((attackPlayer == 'Fire' && attackEnemy == 'Earth') || (attackPlayer == 'Water' && attackEnemy == 'Fire') || (attackPlayer == 'Earth' && attackEnemy == 'Water')) {

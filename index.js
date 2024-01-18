2316edf90064ed4bb019d4a7a8d95bdbdf78a5ed
p3podev <jr_garciaparedes@hotmail.com>
2024-01-18 11:24:22 -0500

    Feat Server up on Github

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const players=[]
+class Player {
+    constructor (id){
+        this.id
+    }
+}
+app.get("/join", (req, res) => {
+    const id=`${Math.random()}`
+    const player= new Player(id)
+    players.push(player)
+    res.setHeader("Access-Control-Allow-Origin","*")
+    res.send(id)
+})
+app.listen(8080, () => {
+    console.log("Server Work")
+})
